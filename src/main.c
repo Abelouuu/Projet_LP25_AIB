5625c4513e48b89a1e65c71a2db7afdabef88136
Abel <131707724+Abelouuu@users.noreply.github.com>
2025-12-09 10:45:38 +0100

    Refactor program options handling and add remote machines

--- src/main.c
+++ src/main.c
@@ -1,19 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 #include "options.h"
+#include "network.h"
+
 
 int main(int argc, char **argv) {
-    struct program_options opts;
+    program_options opts;
     initialiser_options(&opts);
     traiter_options(argc, argv, &opts);
-    valider_options(&opts);
 
     printf("\nOptions traitées avec succès.\n");
     printf("\n======= Options =======\n");
     printf("help: %s\n", opts.help ? "true" : "false");
     printf("dry Run: %s\n", opts.dry_run ? "true" : "false");
-    printf("remote-onfig: %s\n", opts.remote_config ? opts.remote_config : "NULL");
+    printf("remote-c onfig: %s\n", opts.remote_config ? opts.remote_config : "NULL");
     printf("connection-type: %s\n", opts.connection_type ? opts.connection_type : "NULL");
     printf("port: %d\n", opts.port);
     printf("login: %s\n", opts.login ? opts.login : "NULL");
@@ -23,6 +23,36 @@ int main(int argc, char **argv) {
     printf("all: %s\n", opts.all ? "true" : "false");
     printf("=======================\n");
 
+    
+    //==========================création de la liste des machines distantes============================//
+    int nb_machines = 0;
+    remote_machine *liste_machines = NULL;
+    if (opts.remote_config) {
+        lire_config(opts.remote_config, &liste_machines, &nb_machines);
+
+        printf("\n========Liste des machines=======\n\n");
+    } 
+    if(opts.remote_server) {
+        ajouter_machine_utilisateur(&liste_machines, &nb_machines, opts.remote_server, opts.username, opts.password, opts.port, opts.connection_type);
+    }
+    if(nb_machines==0){
+        printf("aucune machine distantes. Lancée le programme en local");
+    } else {
+        //==========================création de la liste des machines distantes============================//
+        for (int i = 0; i < nb_machines; i++)
+            {
+                printf("Machine %d \n", i);
+                printf("nom: \t\t %s \n", liste_machines[i].name);
+                printf("adresse: \t %s \n", liste_machines[i].address);
+                printf("username: \t %s \n", liste_machines[i].username);
+                printf("password: \t %s \n", liste_machines[i].password);
+                printf("type connection: \t %s \n", liste_machines[i].conn_type);
+                printf("port: \t %d \n\n", liste_machines[i].port);
+            }
+
+        // N'oublie pas de libérer la mémoire après
+        free_machine_list(liste_machines, nb_machines);
+    }
+
     return 0;
 }
-
