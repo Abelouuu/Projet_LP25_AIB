d4731dcf0a2667b4b3d031ea33cfb9e27504b8b7
ThunderBasil <imadeddinebetka@gmail.com>
2026-01-05 20:30:15 +0100

    Refactor Process structure and update function signatures
    Updated the Process structure to include CPU percentage and modified function signatures for reading processes and killing processes.

--- include/process.h
+++ include/process.h
@@ -1,46 +1,35 @@
 #ifndef PROCESS_H
 #define PROCESS_H
 
-#include <sys/types.h>  // pour pid_t 
-#include <stddef.h>     // pour size_t
+#include "network.h"
 
 // représentation d'un processus
 typedef struct Process {
     int   pid;               // PID du processus
     char  user[32];          // nom d'utilisateur
-    long  mem_kb;            // mémoire résidente en kB (VmRSS)
+    double cpu_pct;
     double mem_pct;          // pourcentage de la RAM totale (%MEM)
     char  state;             // état (R, S, D, T, Z, etc.)
     char  cmd[256];          // nom de la commande
-    struct Process *next;    // chaînage
+    struct Process *next;    // chaînage pour la liste des processus
 } Process;
 
-// lecture de la liste des processus locaux depuis /proc
-Process *read_processes(void);
+Process *read_proc(int sockfd, ssh_session session);
 
-// calcule le %MEM pour chaque processus (en lisant /proc/meminfo)
-void update_mem_percentage(Process *head);
 
 // trie la liste par %MEM décroissant (plus gourmand en premier)
 Process *sort_by_mem(Process *head);
 
-// affiche les processus (limité à 50 lignes pour éviter le spam)
-void print_processes(Process *head, const char *machine_name);
-
 // Libère la mémoire allouée pour la liste des processus
 void free_processes(Process *processes);
 
-int kill_process_soft(int pid);
-
-int kill_process_hard(int pid);
-
+// Envoie les signaux au pid
+int kill_process_soft(int pid); // Arrêter
+int kill_process_hard(int pid); // Tuer
 int pause_process(int pid);
-
 int continue_process(int pid);
 
-// recherche un processus par PID ou par nom de commande
-
+// Recherche un processus par PID ou par nom de commande
 int processus_recherche(const Process *p, const char *rech);
-
 
 #endif // PROCESS_H
